9ca7214a774605152eb4da8ba07b1a903b5ff8f1
Fredrick Ondieki <fredndk@gmail.com>
2014-08-13 09:50:21 +0300

    Flash Cast Edition Action

---
+++ mavenproject1/src/main/java/dk/i2m/converge/plugins/actions/flashcast/FlashCastEditonAction.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.i2m.converge.plugins.actions.flashcast;
+
+import dk.i2m.converge.core.annotations.OutletAction;
+
+/**
+ *
+ * @author fred
+ */
+@OutletAction
+public class FlashCastEditonAction {
+    
+}
