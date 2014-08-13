06059eed4827cc94468df700ddbae3141aa13b74
Fredrick Ondieki <fredndk@gmail.com>
2014-08-13 10:24:36 +0300

    Flash Cast Edition Action

--- flashcastpluginApi/src/main/java/dk/i2m/converge/plugins/actions/flashcast/FlashCastServicesClient.java
+++ flashcastpluginApi/src/main/java/dk/i2m/converge/plugins/actions/flashcast/FlashCastServicesClient.java
@@ -4,10 +4,37 @@
  */
 package dk.i2m.converge.plugins.actions.flashcast;
 
+import java.util.logging.Logger;
+
 /**
  *
  * @author fred
  */
 public class FlashCastServicesClient {
+    private static final Logger LOG = Logger.getLogger(FlashCastServicesClient.class.getName());
+    private String hostname;
+    private String endpoint;
+    private Integer connectionTimeout = 30000;
+    private Integer socketTimeout = 30000;
+    private String websiteUrl;
+    private String username;
+    private String password;
+    private Integer replyTimeOut=60000;
+    
+     public FlashCastServicesClient() {
+        this("", "", "", "");
+    }
+     public FlashCastServicesClient(String websiteUrl, String username, String password) {
+        this.websiteUrl = websiteUrl;
+        this.username = username;
+        this.password = password;
+    }
+     
+     public FlashCastServicesClient(String websiteUrl, String username, String password,String connectionTimeout) {
+        this.websiteUrl = websiteUrl;
+        this.username = username;
+        this.password = password;
+        this.connectionTimeout=Integer.parseInt(connectionTimeout);
+    }
     
 }
