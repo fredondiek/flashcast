cb5d9c55a3b618a6da262620ea5d0ae53eee7fb9
Fredrick Ondieki <fredndk@gmail.com>
2014-08-13 10:04:22 +0300

    Flash Cast Edition Action

---
+++ flashcastpluginApi/src/main/java/dk/i2m/converge/plugins/actions/flashcast/FlashCastServerConnectionException.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.i2m.converge.plugins.actions.flashcast;
+
+/**
+ *
+ * @author fred
+ */
+public class FlashCastServerConnectionException {
+    
+}
